875d5096a73ace129fefedc71f3dd2ea61dd3775
kolev <kolevone@gmail.com>
2022-06-08 00:14:03 +0300

    adding validation and editing templates.

--- JavaWeb/Moblilelele/src/main/java/com/example/moblilelele/web/UserController.java
+++ JavaWeb/Moblilelele/src/main/java/com/example/moblilelele/web/UserController.java
@@ -1,16 +1,13 @@
 package com.example.moblilelele.web;
 
 import com.example.moblilelele.model.dto.LoginDto;
-import com.example.moblilelele.model.dto.RegisterDto;
 import com.example.moblilelele.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class UserController {
 
 
@@ -20,20 +17,22 @@ public class UserController {
         this.userService = userService;
     }
 
-        @GetMapping("users/login")
-        public String loginPage(){
+    @GetMapping("users/login")
+    public String loginPage() {
         return "auth-login";
-        }
+    }
 
-        @GetMapping("users/logout")
-        public String logout(){
+    @PostMapping("users/login")
+    public String login(LoginDto loginDto) {
+        userService.userLogin(loginDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("users/logout")
+    public String logout() {
         userService.userLogout();
         return "redirect:/";
-        }
+    }
 
-        @GetMapping("users/register")
-        public String userRegister(){
-        return"auth-register";
-        }
 
 }
