2223552018844f0ef0b89fcbc5d71c1c18d1c321
kolev <kolevone@gmail.com>
2022-07-16 23:02:48 +0300

    some javascript
    category options

--- PersonalProject/src/main/resources/static/js/script.js
+++ PersonalProject/src/main/resources/static/js/script.js
@@ -4,6 +4,21 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
-function getValue(sel){
-  alert(sel.value);
+
+const categoryOption = document.getElementById('categoryOption');
+
+
+categoryOption.addEventListener('change', filterByCategory)
+
+
+function filterByCategory() {
+  for (const div of document.getElementsByClassName('filter')) {
+    div.parentElement.style.display='block';
+  };
+  
+  for (const div of document.getElementsByClassName('filter')) {
+    if(div.id!=categoryOption.value){
+      div.parentElement.style.display='none';
+    }
+  };
 }
