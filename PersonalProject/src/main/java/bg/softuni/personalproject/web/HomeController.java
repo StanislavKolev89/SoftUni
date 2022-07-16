3ded1e978a84c9422f83c6c9fa49f60cb5998347
kolev <kolevone@gmail.com>
2022-07-16 23:26:14 +0300

    a
    a

--- PersonalProject/src/main/java/bg/softuni/personalproject/web/HomeController.java
+++ PersonalProject/src/main/java/bg/softuni/personalproject/web/HomeController.java
@@ -1,16 +1,29 @@
 package bg.softuni.personalproject.web;
 
+import bg.softuni.personalproject.service.CategoryService;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.stream.Collectors;
 
 @EnableWebMvc
 @Controller
 public class HomeController {
 
+    private final CategoryService categoryService;
+
+    public HomeController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("/")
-    public String testPage(){
+    public String testPage(Model model){
+        model.addAttribute("firstCategoryItem",categoryService.getAllCategories().get(0));
+        model.addAttribute("allOtherItems",categoryService.getAllCategories().stream().skip(1).collect(Collectors.toList()));
+
         return "index";
     }
 }
--- PersonalProject/src/main/resources/static/js/script.js
+++ PersonalProject/src/main/resources/static/js/script.js
@@ -15,7 +15,7 @@ function filterByCategory() {
   for (const div of document.getElementsByClassName('filter')) {
     div.parentElement.style.display='block';
   };
-  
+
   for (const div of document.getElementsByClassName('filter')) {
     if(div.id!=categoryOption.value){
       div.parentElement.style.display='none';
