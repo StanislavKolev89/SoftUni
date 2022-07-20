9ada947573115e42dcca99ea37782444f2980355
Ivaylo Kolev <ivaylo.kolev.ext@racingpost.com>
2022-07-20 20:32:54 +0300

    Small improvements

--- PersonalProject/src/main/java/bg/softuni/personalproject/service/ShoppingCartService.java
+++ PersonalProject/src/main/java/bg/softuni/personalproject/service/ShoppingCartService.java
@@ -25,35 +25,22 @@ public class ShoppingCartService {
         this.userService = userService;
     }
 
-
     public void addToCart(Long productById, QuantityHolderDTO quantityHolderDTO) {
-        System.out.println();
-        ProductEntity product = productRepository.findById(productById).get();
-      if(hasMatch(productById)){
-          ProductEntity productKey=findProductEntity(productById);
-          cartProducts.replace(productKey,cartProducts.get(productKey)+ quantityHolderDTO.getQuantity());
-      }else{
-          cartProducts.put(product, quantityHolderDTO.getQuantity());
-      }
+        productRepository.findById(productById).ifPresent(productEntity ->
+              cartProducts.computeIfAbsent(productEntity, key -> quantityHolderDTO.getQuantity()));
     }
-
-
 
     public void finishOrder(String principalName) {
         UserEntity buyer = userService.findByName(principalName);
-        orderService.createOrder(cartProducts,buyer);
+        orderService.createOrder(cartProducts, buyer);
         cartProducts.clear();
-    }
-
-    public boolean isEmpty() {
-        return cartProducts.isEmpty();
     }
 
     public BigDecimal findTotalSum() {
         return cartProducts.entrySet().stream()
-                .map(entry -> entry.getKey().getPrice().multiply(BigDecimal.valueOf(entry.getValue())))
-                .reduce(BigDecimal::add)
-                .orElse(BigDecimal.ZERO);
+              .map(entry -> entry.getKey().getPrice().multiply(BigDecimal.valueOf(entry.getValue())))
+              .reduce(BigDecimal::add)
+              .orElse(BigDecimal.ZERO);
 
     }
 
@@ -62,24 +49,14 @@ public class ShoppingCartService {
     }
 
     public void removeProduct(Long productById) {
-        ProductEntity productEntity =null;
-        for (Map.Entry<ProductEntity,Integer> product: cartProducts.entrySet()) {
-            if(product.getKey().getId()==productById){
-                productEntity=product.getKey();
+        ProductEntity productEntity = null;
+        for (Map.Entry<ProductEntity, Integer> product : cartProducts.entrySet()) {
+            if (product.getKey().getId() == productById) {
+                productEntity = product.getKey();
                 break;
             }
         }
 
         cartProducts.remove(productEntity);
-
     }
-
-    private boolean hasMatch(Long productId) {
-        return !cartProducts.entrySet().stream().filter(productEntity -> productEntity.getKey().getId()==productId).collect(Collectors.toList()).isEmpty();
-    }
-
-    private ProductEntity findProductEntity(Long productId) {
-       return cartProducts.entrySet().stream().filter(productEntity -> productEntity.getKey().getId()==productId).collect(Collectors.toList()).get(0).getKey();
-    }
-
 }
