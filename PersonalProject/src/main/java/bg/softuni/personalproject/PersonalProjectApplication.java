0b346a2452de26dfe58b6518b5c2f41c3ebca59c
kolev <kolevone@gmail.com>
2022-07-21 22:49:57 +0300

    Application YML changes
    time zone changes

--- PersonalProject/src/main/java/bg/softuni/personalproject/PersonalProjectApplication.java
+++ PersonalProject/src/main/java/bg/softuni/personalproject/PersonalProjectApplication.java
@@ -3,11 +3,14 @@ package bg.softuni.personalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PersonalProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonalProjectApplication.class, args);
     }
+
+
 
 }
