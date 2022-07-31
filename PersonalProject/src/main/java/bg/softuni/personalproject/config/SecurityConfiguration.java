35dd85cf694686e6a1a48fc456526389488e6437
kolev <kolevone@gmail.com>
2022-07-31 10:56:05 +0300

    a

--- PersonalProject/src/main/java/bg/softuni/personalproject/config/SecurityConfiguration.java
+++ PersonalProject/src/main/java/bg/softuni/personalproject/config/SecurityConfiguration.java
@@ -33,10 +33,12 @@ public class SecurityConfiguration {
                 // everyone can download static resources (css, js, images)
                         requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
                 // everyone can login and register
+                        antMatchers("/", "/users/login","/users/register","/products/users/forSale").permitAll().
                         antMatchers("/", "/users/login","/users/register").permitAll().
-                antMatchers("/products/all","/products/users/forSale").permitAll().
+                antMatchers("/products/all","/products/users/forSale","/contacts").permitAll().
+
                 // pages available only for admins
-                        antMatchers("/admin","/admin/**").hasRole(RoleEnum.ADMIN.name()).
+                        antMatchers("/admin/**").hasRole(RoleEnum.ADMIN.name()).
                 // all other pages are available for logger in users
                         anyRequest().
                 authenticated().
